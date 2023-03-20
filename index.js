48c80dda3cda1bb9ba703279e244298af91f50b4
MarsMwau <marthamumbua16@gmail.com>
2023-03-21 01:13:51 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+//code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ];
+//   const year =record.find(superbowlWin).year;
+// function superbowlWin(record) {
+//     if (record === "W") {
+//         return record.year;
+//     }
+//     // else {
+//     //     return "sadly";
+//     //}
+
+// }
+function superbowlWin(record) {
+    let winningYear;
+    record.find(function(obj) {
+      if (obj.result === "W") {
+        winningYear = obj.year;
+        return true;
+      }
+    });
+    return winningYear;
+  }
+  
